f04b5f66f39e8f50eb6792c1b17ad1cbf30672a6
Pablo Neira Ayuso <pablo@gnumonks.org>
2012-07-03 18:43:42 +0200

    rtp: move RTP header definitions to include rtp.h
    Signed-off-by: Pablo Neira Ayuso <pablo@gnumonks.org>

--- include/osmocom/netif/rtp.h
+++ include/osmocom/netif/rtp.h
@@ -1,6 +1,51 @@
 #ifndef _OSMO_RTP_H_
 #define _OSMO_RTP_H_
 
+/* RTP header as defined by RFC 3550 */
+struct rtp_hdr {
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+	uint8_t  csrc_count:4,
+		 extension:1,
+		 padding:1,
+		 version:2;
+	uint8_t  payload_type:7,
+		 marker:1;
+#elif __BYTE_ORDER == __BIG_ENDIAN
+	uint8_t  version:2,
+		 padding:1,
+		 extension:1,
+		 csrc_count:4;
+	uint8_t  marker:1,
+		 payload_type:7;
+#endif
+	uint16_t sequence;
+	uint32_t timestamp;
+	uint32_t ssrc;
+} __attribute__((packed));
+
+#define RTP_VERSION	2
+
+/* 5.3.1 RTP Header Extension
+ *
+ * If the X bit in the RTP header is one, a variable-length header
+ * extension MUST be appended to the RTP header, following the CSRC list
+ * if present. The header extension contains a 16-bit length field that
+ * counts the number of 32-bit words in the extension, excluding the
+ * four-octet extension header (therefore zero is a valid length).  Only
+ * a single extension can be appended to the RTP data header.
+ */
+struct rtp_x_hdr {
+	uint16_t by_profile;
+	uint16_t length;
+} __attribute__((packed));
+
+/* RTPC header. */
+struct rtcp_hdr {
+	uint8_t byte0;
+	uint8_t type;
+	uint16_t length;
+} __attribute__((packed));
+
 /* XXX: RFC specifies that MTU should used, add generic function to obtain
 	existing MTU. */
 #define RTP_MSGB_SIZE  1500
