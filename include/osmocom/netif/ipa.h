a7d7dd220cd8a94ca6ee341620f0cd40815ec3b7
Harald Welte <laforge@gnumonks.org>
2017-04-14 10:49:18 +0200

    ipa.h: Don't redefine what libosmocore already defines
    Change-Id: Ibd81efc1dc61b8c2019d55a8fa6e3bb99b5acb20

--- include/osmocom/netif/ipa.h
+++ include/osmocom/netif/ipa.h
@@ -1,49 +1,21 @@
 #ifndef _OSMO_NETIF_IPA_H_
 #define _OSMO_NETIF_IPA_H_
 
+#include <osmocom/gsm/protocol/ipaccess.h>
+
+/* This is like 'struct ipaccess_head' in libosmocore, but 'ipa_head' is
+ * actually the more apropriate name, so rather than making more code
+ * use the wrong name, let's keep the duplicate header definitions below */
 struct ipa_head {
 	uint16_t len;	/* network byte order */
 	uint8_t proto;
 	uint8_t data[0];
 } __attribute__ ((packed));
 
-/* IPA protocols. */
-#define IPAC_PROTO_RSL		0x00
-#define IPAC_PROTO_IPACCESS	0xfe
-#define IPAC_PROTO_SCCP		0xfd
-#define IPAC_PROTO_OML		0xff
-#define IPAC_PROTO_OSMO		0xee	/* OpenBSC extension. */
-#define IPAC_PROTO_MGCP_OLD	0xfc	/* OpenBSC extension. */
-
 struct ipa_head_ext {
 	uint8_t proto;
 	uint8_t data[0];
 } __attribute__ ((packed));
-
-/* Protocol extensions. */
-#define IPAC_PROTO_EXT_CTRL	0x00
-#define IPAC_PROTO_EXT_MGCP	0x01
-#define IPAC_PROTO_EXT_LAC	0x02
-
-/* Message types. */
-#define IPAC_MSGT_PING		0x00
-#define IPAC_MSGT_PONG		0x01
-#define IPAC_MSGT_ID_GET	0x04
-#define IPAC_MSGT_ID_RESP	0x05
-#define IPAC_MSGT_ID_ACK	0x06
-#define IPAC_MSGT_SCCP_OLD	0xff	/* OpenBSC extension */
-
-enum ipaccess_id_tags {
-	IPAC_IDTAG_SERNR		= 0x00,
-	IPAC_IDTAG_UNITNAME		= 0x01,
-	IPAC_IDTAG_LOCATION1		= 0x02,
-	IPAC_IDTAG_LOCATION2		= 0x03,
-	IPAC_IDTAG_EQUIPVERS		= 0x04,
-	IPAC_IDTAG_SWVERSION		= 0x05,
-	IPAC_IDTAG_IPADDR		= 0x06,
-	IPAC_IDTAG_MACADDR		= 0x07,
-	IPAC_IDTAG_UNIT			= 0x08,
-};
 
 struct msgb *osmo_ipa_msg_alloc(int headroom);
 void osmo_ipa_msg_push_header(struct msgb *msg, uint8_t proto);
