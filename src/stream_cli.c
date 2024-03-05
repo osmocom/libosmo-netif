osmocom/libosmo-netif

commit 48f9a3c27f8c28c3f59bae553a8ca59ef7ca52b8

stream_cli: Proper handling of send() socket errors
Upon EAGAIN, simply re-enqueue the message and return waiting for next
poll. Upon any other error, force close + reconnect.

Related: OS#6134
Change-Id: I462cb176ebc51f3e99ee796310e8665144c84ccc

src/stream_cli.c
@@ -288,9 +288,15 @@ static int osmo_stream_cli_write(struct osmo_stream_cli *cli)
 	}
 
 	if (ret < 0) {
-		if (errno == EPIPE || errno == ENOTCONN)
-			osmo_stream_cli_reconnect(cli);
-		LOGSCLI(cli, LOGL_ERROR, "received error %d in response to send\n", errno);
+		int err = errno;
+		LOGSCLI(cli, LOGL_ERROR, "send(len=%u) error: %s\n", msgb_length(msg), strerror(err));
+		if (err == EAGAIN) {
+			/* Re-add at the start of the queue to re-attempt: */
+			llist_add(&msg->list, &cli->tx_queue);
+			return 0;
+		}
+		msgb_free(msg);
+		osmo_stream_cli_reconnect(cli);
 	}
 
 	msgb_free(msg);

commit 1eb221a923d4e25747c80359e90e23ccaebc6140

osmo_stream_cli_write(): fix double-free of msgb
Change-Id: I954678acd90593e5311d22a10033c7741f280be3
Fixes: 48f9a3c2 "stream_cli: Proper handling of send() socket errors"

src/stream_cli.c
@@ -303,6 +303,7 @@ static int osmo_stream_cli_write(struct osmo_stream_cli *cli)
 		}
 		msgb_free(msg);
 		osmo_stream_cli_reconnect(cli);
+		return 0;
 	}
 
 	msgb_free(msg);